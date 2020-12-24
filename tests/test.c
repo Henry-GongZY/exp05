a6c9013e08522bf5c1118446a96e84e396f561c0
Henry-GongZY <1811358@mail.nankai.edu.cn>
2020-12-25 00:22:28 +0800

    提交

--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,26 @@
-//第一次测试样例
-int main()
-{
-    int a;
-    int b;
-    char c;
-    b+=(a+b);
+/*
+I'm level 1 test.
+*/
+void main() {
+int a, s;
+a = 10;
+s = 0;
+char ch;
+scanf("%c", ch);
+printf("%c", ch);
+while(a>0 && a<=10 || a%100==10) {
+a -= 1;
+a = 10;
+s += a;
+if(s > -10) {
+printf("result is: %d\n", s);
+int b;
+b = 10;
+int i;
+for(i=0; i<b; i++) {
+printf("Have fun: %d\n", i);
 }
-//first test
+}
+}
+}
+// No more compilation error.
